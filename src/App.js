5f7ce0070f1aa9c4ff19b658231f987503e0b32b
Ameh Solomon Onyeke <amehsolomon46@gmail.com>
2023-03-15 13:24:23 +0100

    End of one lesson

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
 import "./App.css";
 import Hello from "./components/hello";
 import Message from "./components/message";
+import Profile from "./components/profile";
 
 function App() {
   return (
     <div className="App">
       <Hello />
       <Message />
+      <Profile name="Andrew" lastname="Ameh" />
+      <Profile name="Solomon" lastname="Akute" />
     </div>
   );
 }
