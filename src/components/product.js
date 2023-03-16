43bf20d8098f1064b8202b704910851e899eceb1
Ameh Solomon Onyeke <amehsolomon46@gmail.com>
2023-03-16 10:50:45 +0100

    End of lesson 27 - rendering list of objects

--- src/components/product.js
+++ src/components/product.js
@@ -1,6 +1,37 @@
 const Product = () => {
-  const products = ["Laptop", "Phone", "Mifi", "Computer Stand"];
-  const productList = products.map((product) => <h3>{product}</h3>);
+  //   const products = ["Laptop", "Phone", "Mifi", "Computer Stand"];
+  const products = [
+    {
+      id: 1,
+      name: "Laptop",
+      price: 500,
+    },
+    {
+      id: 2,
+      name: "Phone",
+      price: 1000,
+    },
+    {
+      id: 3,
+      name: "Mifi",
+      price: 15000,
+    },
+    {
+      id: 4,
+      name: "Computer Stand",
+      price: 7000,
+    },
+    {
+      id: 5,
+      name: "Laptop",
+      price: 200000,
+    },
+  ];
+  const productList = products.map((product) => (
+    <h3 key={product.id}>
+      {product.name}: ${product.price}
+    </h3>
+  ));
   return <div>{productList}</div>;
 };
 
